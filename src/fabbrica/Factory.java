1b3b184b7afc7048a10cc02bf9e475ee1aab9985
datteroandrea <dattero.andrea@gmail.com>
2021-06-09 19:39:21 +0200

    added comments

--- src/fabbrica/Factory.java
+++ src/fabbrica/Factory.java
@@ -6,6 +6,11 @@ import java.util.function.Function;
 
 public class Factory {
     
+
+    // Function può avere ? super A ma non ? extends A
+    // Function può avere ? extends B ma non ? super B
+    // In sintesi i parametri di un metodo possono essere sussunti (anche se questo java non lo permette nei metodi)
+    // Il valore di ritorno di un metodo può essere subsunto
     public <A,B> List<B> produce(List<? extends A> listA, Function<A,B> f) {
         LinkedList<B> products = new LinkedList<>();
         for(A a : listA) {
