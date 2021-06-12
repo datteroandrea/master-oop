02844eb3970be5f80fdf45a780e21380f05f8cc0
datteroandrea <dattero.andrea@gmail.com>
2021-06-12 10:21:26 +0200

    added RandomSequence.java

--- src/po2/exercises/RandomSequence.java
+++ src/po2/exercises/RandomSequence.java
@@ -3,34 +3,91 @@ package po2.exercises;
 import java.util.Iterator;
 import java.util.Random;
 
-public class RandomSequence implements Iterable<Integer> {
+public abstract class RandomSequence<T> implements Iterable<T> {
 
-    private int length, max;
+    private int length;
     private Random random;
 
-    public RandomSequence(int length, int max) {
+    public RandomSequence(int length) {
         this.length = length;
-        this.max = max;
         this.random = new Random();
     }
 
-    @Override
-    public Iterator<Integer> iterator() {
-        return new Iterator<Integer>(){
-            private int index = 0;
+    public static class RandomIntegerSequence extends RandomSequence<Integer> {
 
-            @Override
-            public boolean hasNext() {
-                return index < length;
-            }
+        private int max;
 
-            @Override
-            public Integer next() {
-                index++;
-                return random.nextInt(max);
-            }
-            
-        };
+        public RandomIntegerSequence(int length, int max) {
+            super(length);
+            this.max = max;
+        }
+
+        @Override
+        public Iterator<Integer> iterator() {
+            return new Iterator<Integer>(){
+                private int index = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return index < RandomIntegerSequence.super.length;
+                }
+
+                @Override
+                public Integer next() {
+                    index++;
+                    return RandomIntegerSequence.super.random.nextInt(RandomIntegerSequence.this.max);
+                }
+                
+            };
+        }
+
+    }
+
+    public static class RandomStringSequence extends RandomSequence<String> {
+
+        private int max;
+        private String seed;
+
+        public RandomStringSequence(int length, int max) {
+            super(length);
+            this.max = max;
+        }
+
+        public RandomStringSequence(int length, int max, String seed) {
+            super(length);
+            this.max = max;
+            this.seed = seed;
+        }
+
+        @Override
+        public Iterator<String> iterator() {
+            return new Iterator<String>(){
+                private int index = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return index < RandomStringSequence.super.length;
+                }
+
+                @Override
+                public String next() {
+                    index++;
+                    String result = "";
+                    if(RandomStringSequence.this.seed == null && RandomStringSequence.this.seed.isEmpty()) {
+                        for(int i = 0; i < max; i++) {
+                            result += RandomStringSequence.super.random.nextInt(200) + 1;
+                        }
+                    } else {
+                        for(int i = 0; i < max; i++) {
+                            result += RandomStringSequence.this.seed.charAt(RandomStringSequence.super.random.nextInt(seed.length()));
+                        }
+                    }
+                    return result;
+                }
+                
+            };
+        }
+
     }
     
 }
